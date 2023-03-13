f1814a2804df8674678fc8552037c9e8c04d342e
Nedas Cerneckis <nedcerneckis@gmail.com>
2023-03-13 23:19:04 +0000

    added model and more markup for recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("Steak", "Medium-Rare steak photo", "https://hips.hearstapps.com/hmg-prod/images/20200109-seo-round-steak-delish-ehg-9267-jpg-1579900169.jpg?crop=1.00xw:0.716xh;0,0.0680xh&resize=1200:*"),
+  ];
 
 }
